4bbbdfb9fc265d9faefe56d3e9f9fe57bf123e20
Jonah Evangelista <jonahevangelista@yahoo.com>
2022-11-18 23:33:33 -0700

    Initial Commit

---
+++ Closet Map/Closet_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Closet_MapApp.swift
+//  Closet Map
+//
+//  Created by Jonah Marie Evangelista on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Closet_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
